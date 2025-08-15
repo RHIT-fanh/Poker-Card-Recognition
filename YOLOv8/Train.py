6f15757f805926fb8946b850f7ef718bcc58b436
jiangy10 <jiangy10@rose-hulman.edu>
2025-08-15 00:13:44 -0700

    trained new model 65e

--- YOLOv8/Train.py
+++ YOLOv8/Train.py
@@ -5,16 +5,16 @@ def main():
     model = YOLO("yolov8n.yaml")
 
     # use the config file 
-    data_yaml_file = "C:/Users/Yuxuan/Desktop/git/Cards-Detection/dataset/data.yaml"
+    data_yaml_file = "dataset/data.yaml"
 
-    project = "C:/Users/Yuxuan/Desktop/git/Cards-Detection"
-    experiment = "Model"
+    project = "."
+    experiment = "Model-65e"
 
     batch_size = 32
 
     # train the model 
     results = model.train(data=data_yaml_file,
-                          epochs=50,
+                          epochs=65,
                           project=project, 
                           name = experiment , 
                           batch = batch_size , 
